769348ccfc76e45b7e51ac5e8d731fff091eea9e
lrubioh <l.rubioh@edu.uah.es>
2025-02-11 18:03:19 +0100

    InvoiceItem

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
 
 import es.uah.matcomp.mp.e1.ejerciciosclases.e3.Employee;
+import es.uah.matcomp.mp.e1.ejerciciosclases.e4.InvoiceItem;
 
 public class Main {
     public static void main(String[] args) {
         // Test constructor and toString()
-        Employee e1 = new Employee(8, "Peter", "Tan", 2500);
-        System.out.println(e1); // toString();
+        InvoiceItem inv1 = new InvoiceItem("A101", "Pen Red", 888, 0.08);
+        System.out.println(inv1); // toString();
         // Test Setters and Getters
-        e1.setSalary(999);
-        System.out.println(e1); // toString();
-        System.out.println("id is: " + e1.getId());
-        System.out.println("firstname is: " + e1.getFirstName());
-        System.out.println("lastname is: " + e1.getLastName());
-        System.out.println("salary is: " + e1.getSalary());
-        System.out.println("name is: " + e1.getName());
-        System.out.println("annual salary is: " + e1.getAnnualSalary()); // Test method
-        // Test raiseSalary()
-        System.out.println(e1.raiseSalary(10));
-        System.out.println(e1);
+        inv1.setQty(999);
+        inv1.setUnitPrice(0.99);
+        System.out.println(inv1); // toString();
+        System.out.println("id is: " + inv1.getId());
+        System.out.println("desc is: " + inv1.getDesc());
+        System.out.println("qty is: " + inv1.getQty());
+        System.out.println("unitPrice is: " + inv1.getUnitPrice());
+        // Test getTotal()
+        System.out.println("The total is: " + inv1.getTotal());
     }
 }
